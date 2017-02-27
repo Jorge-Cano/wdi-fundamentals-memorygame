89932d120d46d8257b46802c85e05cced90af18e
Jorge A. Cano <jacinaustin@gmail.com>
2017-02-27 08:53:08 -0600

    Add files via upload

---
+++ Memory Game Project/main.js
@@ -0,0 +1,12 @@
+//main.js
+
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
